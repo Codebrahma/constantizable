7ea3eed27ddea3ee10b1d37cec46e986be7db38a
Nithin Krishna <nithin@codebrahma.com>
2014-09-27 16:03:21 +0530

    constantizable module created

--- lib/constantizable.rb
+++ lib/constantizable.rb
@@ -1,2 +1,37 @@
-module Constantizable
+module Constantizable 
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def method_missing(method, *args, &block)
+      column_name = @constantized_column
+
+      record = ( 
+                 self.find_by("column_name = ? or column_name = ?", method.to_s, method.to_s.titleize)
+               )
+
+      if record.present?
+        record
+      else
+        super
+      end
+      
+    rescue Exception => e
+      super
+    end
+
+    def constantize_column(column)
+      @constantized_column = column
+    end
+  end
+
+
+  def method_missing(method, *args, &block)
+    if method[-1] == '?'
+      column_name = self.class.instance_variable_get(:@constantized_column)
+      data = self.send(column_name)
+      method[0..-2] == data.titleize.tr(' ','').underscore
+    else
+      super
+    end
+  end
 end
