fe04bf2ffe89f8db889506863f1063e821db36d9
Aboobacker MK <aboobackervyd@gmail.com>
2016-07-04 23:57:46 +0530

    rubocop fixes , renamed shared spec

--- constantizable.gemspec
+++ constantizable.gemspec
@@ -1,25 +1,25 @@
-$:.push File.expand_path("../lib", __FILE__)
+$LOAD_PATH.push File.expand_path('../lib', __FILE__)
 
 # Maintain your gem's version:
-require "constantizable/version"
+require 'constantizable/version'
 
 # Describe your gem and declare its dependencies:
 Gem::Specification.new do |s|
-  s.name        = "constantizable"
+  s.name        = 'constantizable'
   s.version     = Constantizable::VERSION
-  s.authors     = ["Abhishek Sarkar", "Yuvaraja Balamurugan", "Nithin Krishna"]
-  s.email       = ["abhishek@codebrahma.com","yuva@codebrahma.com","nithin@codebrahma.com"]
-  s.summary     = %q{An elegant way to query constant tables, and inquire constant table objects.}
-  s.description = %q{An elegant way to query constant tables, and inquire constant table objects.}
-  s.homepage    = "http://codebrahma.com/constantizable"
-  s.license     = "MIT"
+  s.authors     = ['Abhishek Sarkar', 'Yuvaraja Balamurugan', 'Nithin Krishna']
+  s.email       = ['abhishek@codebrahma.com', 'yuva@codebrahma.com', 'nithin@codebrahma.com']
+  s.summary     = 'An elegant way to query constant tables, and inquire constant table objects.'
+  s.description = 'An elegant way to query constant tables, and inquire constant table objects.'
+  s.homepage    = 'https://github.com/Codebrahma/constantizable'
+  s.license     = 'MIT'
 
-  s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.rdoc"]
-  s.test_files = Dir["test/**/*"]
+  s.files = Dir['{app,config,db,lib}/**/*', 'MIT-LICENSE', 'Rakefile', 'README.rdoc']
+  s.test_files = Dir['test/**/*']
 
-  s.add_dependency "activerecord", "~> 4.2.1"
+  s.add_dependency 'activerecord', '~> 4.2.1'
 
-  s.add_development_dependency "sqlite3"
-  s.add_development_dependency "rspec-rails"
-  s.add_development_dependency "pry"
+  s.add_development_dependency 'sqlite3'
+  s.add_development_dependency 'rspec-rails'
+  s.add_development_dependency 'pry'
 end
--- spec/shared/constantizable.rb
+++ spec/shared/constantizable.rb
@@ -1,71 +1,70 @@
-require "spec_helper"
+require 'spec_helper'
 
-shared_examples "constantizable" do
-
+shared_examples 'constantizable' do
   constantized_columns = described_class.instance_variable_get(:@constantized_columns)
 
-  create_mock_record_with = -> (column_data) {
-    described_class.new({ constantized_columns[0] => column_data })
-  }
+  create_mock_record_with = lambda do |column_data|
+    described_class.new(constantized_columns[0] => column_data)
+  end
 
   context "##{constantized_columns}" do
-    it "returns an string" do
-      record = create_mock_record_with.call("sample")
+    it 'returns an string' do
+      record = create_mock_record_with.call('sample')
       allow(described_class).to receive(:find_by).and_return(record)
       expect(record.send(constantized_columns[0])).to be_an_instance_of String
     end
   end
 
-  context "querying" do
-    context "when #{described_class.to_s} is queried with :#{constantized_columns}" do
+  context 'querying' do
+    context "when #{described_class} is queried with :#{constantized_columns}" do
       it "when :#{constantized_columns} contains single word" do
-        record = create_mock_record_with.call("sample")
+        record = create_mock_record_with.call('sample')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample).to eq record
       end
 
       it "when :#{constantized_columns} contains more than 1 word without space" do
-        record = create_mock_record_with.call("sample_name")
+        record = create_mock_record_with.call('sample_name')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample_name).to eq record
       end
 
       it "when :#{constantized_columns} contains more than 1 word with space" do
-        record = create_mock_record_with.call("sample name")
+        record = create_mock_record_with.call('sample name')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample_name).to eq record
       end
 
       it "when :#{constantized_columns} contains more than 1 word with space and upper case" do
-        record = create_mock_record_with.call("Sample Name")
+        record = create_mock_record_with.call('Sample Name')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample_name).to eq record
       end
     end
   end
 
-  context "inquiry" do
-    context "when an instance of #{described_class.to_s} is inquired with :#{constantized_columns}?" do
+  context 'inquiry' do
+    context "when an instance of #{described_class} is inquired with :#{constantized_columns}?" do
       it "when :#{constantized_columns} contains single word" do
-        record = create_mock_record_with.call("sample")
+        record = create_mock_record_with.call('sample')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample.sample?).to eq true
       end
 
       it "when :#{constantized_columns} contains more than 1 word without space" do
-        record = create_mock_record_with.call("sample_name")
+        record = create_mock_record_with.call('sample_name')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample_name.sample_name?).to eq true
       end
 
       it "when :#{constantized_columns} contains more than 1 word with space" do
-        record = create_mock_record_with.call("sample name")
+        record = create_mock_record_with.call('sample name')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample_name.sample_name?).to eq true
       end
 
       it "when :#{constantized_columns} contains more than 1 word with space and upper case" do
-        record = create_mock_record_with.call("Sample Name")
+        record = create_mock_record_with.call('Sample Name')
         allow(described_class).to receive(:find_by).and_return(record)
         expect(described_class.sample_name.sample_name?).to eq true
       end
